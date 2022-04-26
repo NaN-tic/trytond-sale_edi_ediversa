218bcb5dd7f48ab4aa0898db30ca0f96b77f4844
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:21:13 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part sale_edi_ediversa module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_sale_edi_ediversa import suite
-
-__all__ = ['suite']
