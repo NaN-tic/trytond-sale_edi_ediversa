9e9345c5f0c12061c37396c210195e847e104567
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 10:44:55 +0200

    Pyflakes fixes

--- party.py
+++ party.py
@@ -1,7 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta, Pool
+from trytond.pool import PoolMeta
 
 
 class Party(metaclass=PoolMeta):
